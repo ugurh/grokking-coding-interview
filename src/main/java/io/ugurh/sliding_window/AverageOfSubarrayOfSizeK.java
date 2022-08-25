882531417fb51773dcc639878df34be82349f96e
harun ugur <ugur.harun@yandex.com>
2022-08-25 04:25:41 +0300

    Find the average of each subarray of ‘K’ contiguous elements in an array

---
+++ src/main/java/io/ugurh/sliding_window/AverageOfSubarrayOfSizeK.java
@@ -0,0 +1,53 @@
+package io.ugurh.sliding_window;
+
+import java.util.Arrays;
+
+import static java.lang.System.*;
+
+public class AverageOfSubarrayOfSizeK {
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 3, 2, 6, -1, 4, 1, 8, 2};
+        int slice = 5;
+        double[] result = AverageOfSubarrayOfSizeK.findAverages(slice, arr);
+        out.println("Averages of subarrays of size K: " + Arrays.toString(result));
+
+        double[] result2 = AverageOfSubarrayOfSizeK.findAverages2(slice, arr);
+        out.println("Averages of subarrays of size K: " + Arrays.toString(result2));
+    }
+
+    /**
+     * Time complexity:
+     * Since for every element of the input array, we are calculating the sum of its next ‘K’ elements,
+     * the time complexity of the above algorithm will be O(N*K)
+     * where ‘N’ is the number of elements in the input array.
+     */
+    private static double[] findAverages(int slice, int[] arr) {
+        double[] result = new double[arr.length - slice + 1];
+        for (int i = 0; i <= arr.length - slice; i++) { //O(n)
+            int subTotal = 0;
+            for (int j = i; j < i + slice; j++) { //O(k)
+                subTotal += arr[j];
+            }
+            double avg = (double) subTotal / slice;
+            result[i] = avg;
+        }
+        return result;
+    }
+
+    // the algorithm complexity will reduce to O(N)
+    private static double[] findAverages2(int slice, int[] arr) {
+        double[] result = new double[arr.length - slice + 1];
+        int subTotal = 0;
+        int k = 0;
+        for (int i = 0; i < arr.length; i++) { //O(n)
+            subTotal += arr[i];
+            if (i >= arr.length - slice) {
+                result[k] = (double) subTotal / slice;
+                subTotal -= arr[k];
+                k++;
+            }
+        }
+        return result;
+    }
+}
